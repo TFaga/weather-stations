b000095e20f7b456b53db52f75930d99edfe7ac7
Tilen Faganel <tilen.faganel@me.com>
2014-12-31 01:57:21 +0100

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var livereload = require('connect-livereload'),
+	path = require('path');
+
+var appServer = function () {
+    return require('./server/server');
+};
+
+module.exports = function (grunt) {
+
+	// show elapsed time at the end
+    require('time-grunt')(grunt);
+    
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    var yeomanConfig = {
+        app: 'client',
+        dist: 'dist'
+    };
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        connect: {
+            options: {
+                port: 3000,
+                hostname: '0.0.0.0'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            appServer(),
+                            livereload({port: 35729}),
+                            connect.static(path.resolve(yeomanConfig.app))
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            appServer(),
+                            connect.static(path.resolve(yeomanConfig.app))
+                        ];
+                    }
+                }
+            }
+        },
+        watch: {
+            livereload: {
+                options: {
+                    livereload: 35729
+                },
+                files: [
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask('serve', function (target) {
+
+        grunt.task.run([
+            'connect:livereload',
+            'watch'
+        ]);
+    });
+}
