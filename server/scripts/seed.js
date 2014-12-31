ddb77f180d037e8f29e4214c70a8b1c57fea988f
Tilen Faganel <tilen.faganel@me.com>
2014-12-31 01:11:59 +0100

    Added data seed script

---
+++ server/scripts/seed.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var app = require('../server'),
+	loopback = require('loopback'),
+	User = loopback.getModel("user"),
+	Role = loopback.getModel("role");
+
+User.findOrCreate(
+	{where: {email: 'tilen.faganel@me.com' }},
+	{
+		username: 'faga',
+		firstname: 'Tilen',
+		lastname: 'Faganel',
+		email: 'tilen.faganel@me.com',
+		password: '123456',
+		emailVerified: true
+	},
+	function(err, user) {
+		if(err) throw err;
+
+		console.log('Created user: ', user.username);
+
+		process.exit(0);
+	}
+)
+
