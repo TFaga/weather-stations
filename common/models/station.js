9f1b4898cd09367602701b694209169825d1d33c
Tilen Faganel <tilen.faganel@me.com>
2015-01-05 13:24:17 +0100

    Disabled station relation edit methods

--- common/models/station.js
+++ common/models/station.js
@@ -5,4 +5,9 @@ module.exports = function(Station) {
 	Station.disableRemoteMethod('upsert', true);
 	Station.disableRemoteMethod('prototype.updateAttributes', true);
 	Station.disableRemoteMethod('updateAll', true);
+
+  	Station.disableRemoteMethod('__create__measurements', false);
+  	Station.disableRemoteMethod('__delete__measurements', false);
+  	Station.disableRemoteMethod('__destroyById__measurements', false);
+  	Station.disableRemoteMethod('__updateById__measurements', false);
 };
