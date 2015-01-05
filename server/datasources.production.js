00baa7a0cbec885e233ccc284c15766185c7dfda
Tilen Faganel <tilen.faganel@me.com>
2015-01-05 16:07:38 +0100

    Added production specific files

---
+++ server/datasources.production.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    connector: "mssql",
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    options: {
+	   encrypt: true
+	}
+  }
+};
