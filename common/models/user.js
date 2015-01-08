2dac715ca247d096dfbbe94d4fe31cf57e24bd1d
Tilen Faganel <tilen.faganel@me.com>
2015-01-08 16:34:58 +0100

    Disabled unnecessary remote methods on the user model

--- common/models/user.js
+++ common/models/user.js
@@ -2,4 +2,10 @@ module.exports = function(User) {
 
 	require('../../server/mixins/maxlimit')(User, 100);
 	require('../../server/mixins/timestamps')(User);
+
+  User.disableRemoteMethod('resetPassword', true);
+  User.disableRemoteMethod('confirm', true);
+
+  User.disableRemoteMethod('__create__accessTokens', false);
+  User.disableRemoteMethod('__updateById__accessTokens', false);
 };
