1f5e8b4f69971c08320328addfd017ed9acddf6e
Tilen Faganel <tilen.faganel@me.com>
2015-01-09 19:40:17 +0100

    Optimized timestamps security

--- server/mixins/timestamps.js
+++ server/mixins/timestamps.js
@@ -13,20 +13,13 @@ module.exports = function timestamps(Model) {
     }
   };
 
+  Model.setter.created = function(created) {
+    if (this.isNewRecord())
+      this.$created = created;
+  };
+
   Model.applyTimestamps = function(data, creation) {
     data.modified = new Date();
     if (creation) data.created = data.modified;
   };
-
-  Model.removeCreatedProperty = function(ctx, model, next) {
-    var body = ctx.req.body;
-    if (body && body.created) {
-      delete body.created;
-    }
-    next();
-  };
-
-  Model.beforeRemote('upsert', Model.removeCreatedProperty);
-  Model.beforeRemote('updateAll', Model.removeCreatedProperty);
-  Model.beforeRemote('prototype.updateAttributes', Model.removeCreatedProperty);
 };
