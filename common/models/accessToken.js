af1df4d5234da37bbe97f072ec8e4711c0170a6f
Tilen Faganel <tilen.faganel@me.com>
2015-01-09 22:19:25 +0100

    Added missing validation to the access token model

--- common/models/accessToken.js
+++ common/models/accessToken.js
@@ -1,6 +1,7 @@
 module.exports = function(AccessToken) {
 
   AccessToken.validatesLengthOf('ttl', {max: 2419200, message: {max: 'is too big'}});
+  AccessToken.validatesNumericalityOf('ttl', {int: true});
 
   AccessToken.findForRequest = function(req, options, cb) {
     if (cb === undefined && typeof options === 'function') {
