7027ceae2065f08dde371eac8a8fd965c9ab2ed6
Tilen Faganel <tilen.faganel@me.com>
2015-01-09 10:39:05 +0100

    Updated the seed script to include the admin role

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -3,7 +3,8 @@
 var app = require('../server'),
 	loopback = require('loopback'),
 	User = loopback.getModel('user'),
-	Role = loopback.getModel('role');
+	Role = loopback.getModel('role')
+  RoleMapping = loopback.getModel('roleMapping');
 
 User.findOrCreate(
 	{where: {email: 'tilen.faganel@me.com' }},
@@ -19,7 +20,44 @@ User.findOrCreate(
 
 		console.log('Created user: ', user.username);
 
-		process.exit(0);
-	}
+    Role.findOrCreate(
+      {where: {name: 'admin'}},
+      {
+        name: 'admin',
+        description: 'Administrator'
+      }, function(err, role) {
+        if (err) throw err;
+
+        console.log('Created role: ', role.name);
+
+        role.principals(
+          {
+            where: {
+              principalType: RoleMapping.USER,
+              principalId: user.id
+            }
+          },
+          function(err, principals) {
+            if(err) throw err;
+
+            if (principals.length === 0) {
+              role.principals.create(
+                {
+                  principalType: RoleMapping.USER,
+                  principalId: user.id
+                }, function(err, principal) {
+                  if(err) throw err;
+
+                  process.exit(0);
+                }
+              );
+            } else {
+              process.exit(0);
+            }
+          }
+        )
+      }
+    );
+  }
 );
 
