3c188e479d2579e8ad475399ec60967b89c722eb
Tilen Faganel <tilen.faganel@me.com>
2015-01-09 22:20:30 +0100

    Added missing validations to role mappings

--- common/models/roleMapping.js
+++ common/models/roleMapping.js
@@ -1,7 +1,14 @@
 module.exports = function(RoleMapping) {
 
-   RoleMapping.validatesUniquenessOf('roleId', {
-     message: 'User already has the requested role',
-     scopedTo: ['principalType', 'principalId']
-   });
+  RoleMapping.validateReference = require('../../server/mixins/validateReference');
+
+  RoleMapping.validateReference(RoleMapping, 'roleId', 'role', 'doesn\'t exist');
+  RoleMapping.validateReference(RoleMapping, 'principalId', 'user', 'doesn\'t exist');
+  RoleMapping.validatesUniquenessOf('roleId', {
+    message: 'User already has the requested role',
+    scopedTo: ['principalType', 'principalId']
+  });
+  RoleMapping.validatesInclusionOf('principalType', {
+    in: [RoleMapping.USER], message: 'is not allowed'
+  });
 }
