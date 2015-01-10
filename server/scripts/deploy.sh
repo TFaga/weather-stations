07b3c2fbffff7427aeb9a12e7204b1bcac568f03
Tilen Faganel <tilen.faganel@me.com>
2015-01-10 13:34:30 +0100

    Added deploy settings for the staging server

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
+git config --global user.email "travis@travis-ci.org";
+git config --global user.name "travis";
+
+# Deploy to staging server
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG == "" ]];then
+
+  git remote add azure https://$AZ_USER:$AZ_PASS@$AZ_STAGING.scm.azurewebsites.net:443/$AZ_STAGING.git;
+
+  git push -f azure master > /dev/null;
+fi
+
 # Deploy to beta server
 if [[ $TRAVIS_TAG != "" && $TRAVIS_PULL_REQUEST == "false" ]];then
-
-	git config --global user.email "travis@travis-ci.org";
-	git config --global user.name "travis";
 
 	git remote add azure https://$AZ_USER:$AZ_PASS@$AZ_BETA.scm.azurewebsites.net:443/$AZ_BETA.git;
 
