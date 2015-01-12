043de389b45f888ffd177a572e798947b87e4fc1
Tilen Faganel <tilen.faganel@me.com>
2015-01-12 16:23:43 +0100

    Refactored maxLimit mixin

--- server/mixins/maxlimit.js
+++ server/mixins/maxlimit.js
@@ -4,15 +4,7 @@ module.exports = function maxlimit(Model, limit) {
 
     if (ctx.methodString.indexOf('__get__') > -1) {
 
-      if (ctx.args.filter) {
-
-        if (!ctx.args.filter.limit || ctx.args.filter.limit > limit) {
-          ctx.args.filter.limit = limit;
-        }
-
-      } else {
-        ctx.args.filter = { limit: limit };
-      }
+      Model.maxLimit(ctx);
     }
 
     next();
@@ -20,16 +12,27 @@ module.exports = function maxlimit(Model, limit) {
 
   Model.beforeRemote('find', function(ctx, model, next) {
 
+    Model.maxLimit(ctx);
+
+    next();
+  });
+
+  Model.maxLimit = function(ctx) {
+
+    if (typeof ctx.args.filter == 'string') {
+      try {
+        ctx.args.filter = JSON.parse(ctx.args.filter);
+      } catch(e) {}
+    }
+
     if (ctx.args.filter) {
 
-      if (!ctx.args.filter.limit || ctx.args.filter.limit > limit) {
+      if ((!ctx.args.filter.limit || ctx.args.filter.limit > limit) && typeof ctx.args.filter != 'string') {
         ctx.args.filter.limit = limit;
       }
 
     } else {
       ctx.args.filter = { limit: limit };
     }
-
-    next();
-  });
+  }
 };
