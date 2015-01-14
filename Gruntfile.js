1c690e53ee27543d245d4016312c10ea4e19d0f7
Tilen Faganel <tilen.faganel@me.com>
2015-01-14 09:28:02 +0100

    Updated grunt with frontend serve

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var livereload = require('connect-livereload'),
-	path = require('path');
+    path = require('path');
 
 var appServer = function () {
     return require('./server/server');
@@ -10,76 +10,155 @@ var appServer = function () {
 module.exports = function (grunt) {
 
 	// show elapsed time at the end
-    require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
-    // load all grunt tasks
-    require('load-grunt-tasks')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
 
-    var yeomanConfig = {
-        app: 'client',
-        dist: 'dist'
-    };
+  var yeomanConfig = {
+      app: 'client',
+      dist: 'public'
+  };
 
-    grunt.initConfig({
-        yeoman: yeomanConfig,
-        connect: {
+  grunt.initConfig({
+      yeoman: yeomanConfig,
+      connect: {
+          options: {
+            port: 3000,
+            hostname: '0.0.0.0'
+          },
+          livereload: {
             options: {
-                port: 3000,
-                hostname: '0.0.0.0'
-            },
-            livereload: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            appServer(),
-                            livereload({port: 35729}),
-                            connect.static(path.resolve(yeomanConfig.app))
-                        ];
-                    }
-                }
-            },
-            test: {
-                options: {
-                    middleware: function (connect) {
-                        return [
-                            appServer(),
-                            connect.static(path.resolve(yeomanConfig.app))
-                        ];
-                    }
-                }
+              middleware: function (connect) {
+                return [
+                  appServer(),
+                  livereload({port: 35729}),
+                  connect.static(path.resolve('.tmp')),
+                  connect.static(path.resolve(yeomanConfig.app))
+                ];
+              }
             }
-        },
-        watch: {
-            livereload: {
-                options: {
-                    livereload: 35729
-                },
-                files: [
-                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-                ]
-            }
-        },
-        jshint: {
+          },
+          test: {
             options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            all: [
-                'Gruntfile.js',
-                'server/{,*/}*.js'
-            ]
+              middleware: function (connect) {
+                return [
+                  appServer(),
+                  connect.static(path.resolve('.tmp')),
+                  connect.static(path.resolve(yeomanConfig.app))
+                ];
+              }
+            }
+          }
+      },
+      watch: {
+        options: {
+          livereload: 35729
+        },
+        index: {
+          files: [
+            '<%= yeoman.app %>/*.html'
+          ],
+          tasks: ['targethtml']
+        },
+        react: {
+          files: ['<%= yeoman.app %>/scripts/**/*.js'],
+          tasks: ['browserify:dev']
+        },
+        compass: {
+          files: ['<%= yeoman.app %>/scripts/**/*.{scss,sass}'],
+          tasks: ['compass:serve', 'autoprefixer:dev']
+        },
+        images: {
+          files: [
+            '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          ]
         }
-    });
+      },
+      clean: {
+        dist: ['.tmp', '<%= yeoman.dist %>/*'],
+        serve: '.tmp'
+      },
+      targethtml: {
+        dist: {
+          files: {
+            '.tmp/index.html': '<%= yeoman.app %>/index.html'
+          }
+        }
+      },
+      browserify: {
+        options: {
+          transform: ['reactify']
+        },
+        dist: {
+          files: {
+            '.tmp/scripts/app.js': 'client/scripts/app.js',
+          },
+        },
+        dev: {
+          files: {
+            '.tmp/scripts/app.js': 'client/scripts/app.js',
+          },
+          options: {
+            browserifyOptions: {
+              debug: true
+            }
+          }
+        }
+      },
+      compass: {
+        options: {
+          sassDir: '<%= yeoman.app %>/scripts/components',
+          cssDir: '.tmp/styles',
+          specify: '<%= yeoman.app %>/scripts/components/layout.scss',
+          imagesDir: '<%= yeoman.app %>/images',
+          javascriptsDir: '<%= yeoman.app %>/scripts',
+          fontsDir: '<%= yeoman.app %>/fonts',
+          relativeAssets: true
+        },
+        dist: {},
+        serve: {
+          options: {
+            debugInfo: true
+          }
+        }
+      },
+      autoprefixer: {
+        dev: {
+          expand: true,
+          src: '.tmp/styles/*.css'
+        },
+        dist: {
+          expand: true,
+          src: '<%= yeoman.dist %>/styles/*.css'
+        }
+      },
+      jshint: {
+          options: {
+              jshintrc: '.jshintrc',
+              reporter: require('jshint-stylish')
+          },
+          all: [
+              'Gruntfile.js',
+              'server/{,*/}*.js'
+          ]
+      }
+  });
 
-    grunt.registerTask('serve', [
-		'connect:livereload',
-        'watch'
-    ]);
+  grunt.registerTask('serve', [
+    'clean:serve',
+    'targethtml',
+    'browserify:dev',
+    'compass:serve',
+    'autoprefixer:dev',
+    'connect:livereload',
+    'watch'
+  ]);
 
-    grunt.registerTask('test', [
-    	'jshint'
-    ]);
+  grunt.registerTask('test', [
+  	'jshint'
+  ]);
 
-    grunt.registerTask('build', [
-    ]);
+  grunt.registerTask('build', [
+  ]);
 };
