9e45d9cc4277e4d60bd8d723d0f12020013c0854
Tilen Faganel <tilen.faganel@me.com>
2015-01-19 14:55:47 +0100

    Added specific components for bootstrap to work with react-router

--- client/scripts/components/header.jsx
+++ client/scripts/components/header.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import {Navbar, Nav, NavItem, DropdownButton, MenuItem} from 'react-bootstrap'
 import Router, {Link} from 'react-router'
+import {NavItemLink} from 'react-router-bootstrap'
 
 export default React.createClass({
 
   render() {
 
+    var Logo = (
+      <div>
+        <span>
+          <img className="header-logo" src="images/Logo.png" />
+          <a>Vremenske postaje</a>
+        </span>
+      </div>
+    )
+
     return (
-      <header>
-        <nav className="navbar navbar-weather navbar-static-top" role="navigation">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <Link to="/" className="navbar-brand">Weather stations</Link>
-            </div>
-          </div>
-        </nav>
-      </header>
+      <Navbar fluid inverse staticTop toggleNavKey={1} brand={Logo}>
+        <Nav pullRight eventKey={1}>
+          <NavItemLink to="layout">Prijava</NavItemLink>
+        </Nav>
+      </Navbar>
     )
   }
 })
