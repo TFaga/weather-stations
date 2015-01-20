e0b36e08d8368aa1af015d49f383da81eeb6c4f2
Tilen Faganel <tilen.faganel@me.com>
2015-01-20 10:48:49 +0100

    Added google maps marker with label

--- client/scripts/components/home/home.jsx
+++ client/scripts/components/home/home.jsx
@@ -3,15 +3,29 @@ import React from 'react'
 export default React.createClass({
 
   componentDidMount() {
-    var mapOptions = {
+    // Create the map
+    let map = new google.maps.Map(this.getDOMNode(), {
       zoom: 9,
       center: new google.maps.LatLng(46.119944, 14.815333),
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+    });
 
-    var map = new google.maps.Map(this.getDOMNode(), mapOptions);
+    this.setState({ map: map });
 
-    this.setState( { map : map } );
+    this.renderMarkers([{}]);
+  },
+
+  renderMarkers(stations) {
+    stations.forEach(station => {
+      let marker1 = new MarkerWithLabel({
+        position: new google.maps.LatLng(46.119944, 14.815333),
+        map: this.state.map,
+        labelContent: "$425K",
+        labelAnchor: new google.maps.Point(22, 0),
+        labelClass: "labels",
+        labelStyle: {opacity: 0.75}
+      });
+    });
   },
 
   render() {
